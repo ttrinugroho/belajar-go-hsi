b04936814a3b93e26a4ec5cf7a3de56b7775dc3d
Teguh Tri Nugroho <ttrinugrohodev@gmail.com>
2025-07-26 11:01:47 +0700

    (fix:tugas-pertemuan-3): add closure function

--- tugas-pertemuan-3/main.go
+++ tugas-pertemuan-3/main.go
@@ -6,30 +6,31 @@ import (
 	"tugas-pertemuan-3/mahasiswa"
 )
 
-func main() {
+func sumUmur() (*int, func(int)) {
 	umur := 0
+	return &umur, func(i int) {
+		umur += i
+	}
+}
+
+func main() {
+	umur, add := sumUmur()
 
 	ali := mahasiswa.BuatMahasiswa("ali", 20, 98, 93, 77, 82, 83)
 	fmt.Println(ali.Info())
 	fmt.Println("Rata-Rata nilai: " + strconv.FormatFloat(ali.RataRata(), 'f', 2, 64))
 
-	func(u int) {
-		umur += u
-	}(ali.GetUmur())
-
+	add(ali.GetUmur())
 	fmt.Println("---")
 
 	siti := mahasiswa.BuatMahasiswa("Siti", 22, 90, 99, 90, 99, 90, 99)
 	fmt.Println(siti.Info())
 	fmt.Println("Rata-Rata nilai: " + strconv.FormatFloat(siti.RataRata(), 'f', 2, 64))
 
-	func(u int) {
-		umur += u
-	}(siti.GetUmur())
-
+	add(siti.GetUmur())
 	fmt.Println("---")
 
 	fmt.Println("Version Package:", mahasiswa.Version)
 	fmt.Println("Nilai Maksimum:", strconv.Itoa(mahasiswa.GetMaxNilai()))
-	fmt.Println("Total umur Mahasiswa:", strconv.Itoa(umur))
+	fmt.Println("Total umur Mahasiswa:", strconv.Itoa(*umur))
 }
